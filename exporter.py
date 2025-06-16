d20df7d6a223c75f0fc92f792609fa9a5c66fc2d
Xander <xheksteen@gmail.com>
2025-06-15 21:58:16 -0700

    Added comments to confusing parts in exporter.py

--- exporter.py
+++ exporter.py
@@ -45,9 +45,13 @@ def export_to_json(file_name, tracks):
     path = os.path.join(export_dir, f"{file_name}.json")
     with open(path, "w", encoding="utf-8") as f:
         
+        # This is a shortcut to reformat our simple list of tracks.
+        # It creates a new list where each song is structured with "track" and "artist" labels.
         track_list = [
             {"track": track_name, "artist":artist_name} for track_name, artist_name in tracks
         ]
+        # This writes the structured list to the file. 
+        # The "indent=2" part makes the file nicely formatted and easy for humans to read.
         json.dump(track_list, f, indent=2)
 
     logging.info("Saved %d tracks to %s.json", len(track_list), file_name)
@@ -66,10 +70,14 @@ def export_to_csv(file_name, tracks):
     """
     
     path = os.path.join(export_dir, f"{file_name}.csv")
+    # We add newline="" to prevent blank rows from appearing in the saved CSV file.
     with open(path, "w", newline="",  encoding="utf-8") as f:
 
+        # This sets up a "writer" that knows how to properly format data for a CSV file.
         writer = csv.writer(f)
+        # ".writerow" writes a single row. We use it here for the column headers.
         writer.writerow(["Track", "Artist"])
+        # ".writerows" writes all the items from our track list at once.
         writer.writerows(tracks)
 
         logging.info("Saved tracks to %s.csv", file_name)
